29344e5421c964dec651c732eabead8a79dca606
Adrian <ad.rivarola18@gmail.com>
2024-09-24 21:34:46 -0500

    loading

--- client/src/components/FoodDisplay/FoodDisplay.jsx
+++ client/src/components/FoodDisplay/FoodDisplay.jsx
@@ -1,36 +1,42 @@
-import React, { useContext, useMemo } from 'react';
-import './FoodDisplay.css';
-import { StoreContext } from '../../context/StoreContext';
-import FoodItem from '../FoodItem/FoodItem';
+import React, { useContext, useMemo } from "react";
+import "./FoodDisplay.css";
+import { StoreContext } from "../../context/StoreContext";
+import FoodItem from "../FoodItem/FoodItem";
 
 const FoodDisplay = ({ category }) => {
+  const { food_list, search } = useContext(StoreContext);
 
-  const { food_list, search } = useContext( StoreContext );
-
-  
   return (
-    <div className='food-display' id='food-display'>
+    <div className="food-display" id="food-display">
       <h2>Top dishes near you</h2>
       <div className="food-display-list">
-        { food_list.map(( item, index ) => {
-          if(
-            (category==="All" || category===item.category) &&
-            (item.name.toLowerCase().includes(search.toLowerCase()))
-          ){
-            return (
-              <FoodItem 
-              key={index} 
-              id={item._id} 
-              name={item.name} 
-              description={item.description} 
-              price={item.price} 
-              image={item.image} 
-              />)
+        {
+          food_list.length < 1
+          ? 
+            <h4 className="loading">Loading...</h4>
+          : 
+          food_list.map((item, index) => {
+            if (
+              (category === "All" || category === item.category) &&
+              item.name.toLowerCase().includes(search.toLowerCase())
+            ) {
+              return (
+                <FoodItem
+                  key={index}
+                  id={item._id}
+                  name={item.name}
+                  description={item.description}
+                  price={item.price}
+                  image={item.image}
+                />
+              );
+            }
+          })
           }
-        })}
+        
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default FoodDisplay
+export default FoodDisplay;
